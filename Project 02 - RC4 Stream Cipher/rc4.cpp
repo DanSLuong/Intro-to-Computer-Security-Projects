2f05696c5bef2a060c76253fc71366e50e1860bd
DanSLuong <hayate58@gmail.com>
2018-07-22 04:06:42 -0500

    Updated - First real commit

---
+++ Project 02 - RC4 Stream Cipher/rc4.cpp
@@ -0,0 +1,131 @@
+// Danny Luong
+// Project 02 - RC4
+// CS 3318 - Introduction to Computer Security
+
+// Implement the RC4 stream cipher in C++.
+// User should be able to enter any key that is 5 bytes to 32 bytes long.
+// Be sure to discard the first 3072 bytes of the pseudo random numbers.
+// THE KEY OR THE INPUT TEXT MUST NOT BE HARD CODED IN THE PROGRAM.
+// You should write TWO separate programs: encryption and decryption.
+// The encryption program should input the plaintext file and output a
+// cipher text in hex. The decryption program should input the cipher
+// text file in hex and output the plaintext.
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+// unsigned char S[256];
+// unsigned int i, j;
+
+void KSA(char key)
+{
+    char S[256];
+
+    int temp_i = 0;
+    int temp_j = 0;
+
+    int key_length = 0;
+    key_length = key.length();
+
+    for (int i = 0; i < 256; i++)
+    {
+        //S[i] := i
+        S[i] = i;
+    }
+    //j := 0 
+    int j = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        // j:=(j+S[i]+key[i%key_length]%256)
+        j = (j + S[i] + key[i % key_length] % 256);
+
+        // swap values of S[i] and S[j]
+        temp_i=S[i];
+        temp_j=S[j];
+        S[i]=temp_j;
+        S[j]=temp_i;
+    }
+}
+
+char PRGA(char S[])
+{
+    // i := 0 
+    int i = 0;
+    // j := 0 
+    int j = 0;
+
+    int temp_i = 0;
+    int temp_j = 0;
+
+    // while GeneratingOutput:
+    while (K)
+    {
+        // i := (i + 1) % 256
+        i = (i + 1) % 256;
+
+        // j := (j + S[i]) % 256
+        j = (j + S[i]) % 256;
+
+        //swap values of S[i] and S[j]
+        temp_i=S[i];
+        temp_j=S[j];
+        S[i]=temp_j;
+        S[j]=temp_i;
+        
+        //K := S[(S[i] + S[j]) % 256]
+        K = S[(S[i] + S[j]) % 256];
+
+        //output K
+        return K;
+    }
+}
+
+char keystream(char key){
+    char S[256];
+    S[]=KSA(key);
+    return PRGA(S);
+}
+
+
+// This function encrypts the Caesar Cipher
+string encrypt(string plaintext, char S)
+{
+    string result = "";
+
+
+    return result;
+}
+
+// This function decrypts the ciphertext
+string decrypt(string ciphertext, char S)
+{
+    string result = "";
+
+
+    return result;
+}
+
+int main()
+{
+    ifstream inputFile;
+    string fileName;
+    char ch;
+    string userKey;
+
+    // Get the file name of the plaintext
+    cout << "Enter the plaintext file name: " << endl;
+    cin >> fileName;
+    cout << endl;
+
+    // User declared key between 5 bytes to 32 bytes long
+    cout << "Enter any key between 5 bytes to 32 bytes: " << endl;
+    cin >> userKey;
+    cout << endl;
+
+    // Open the designated plaintext file
+    inputFile.open(fileName.c_str(), ios::in);
+}
+
